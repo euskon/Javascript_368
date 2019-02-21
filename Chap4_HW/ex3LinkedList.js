6cb87ed4c1c70c705c8d656089aeda788b35ad65
Zach Freund <theeuskon@gmail.com>
2019-02-21 15:49:43 -0600

    edits before 645

--- Chap4_HW/ex3LinkedList.js
+++ Chap4_HW/ex3LinkedList.js
@@ -7,6 +7,14 @@ function nth(list, num){
     return(nth(list.rest, num -1));
   }
 }
+arr1 = [5,6]
 function prepend(element, subList){
-  let list = {value:
-function arrayTolist(...arr){
+  let new_root = {value: element, rest: subList};//rest: {subList}
+  return new_root;
+}
+function arrayTolist(arr){
+  let list = null;
+  for(let i = (arr.length - 1); i >= 0; i--){
+     let node = {value: arr[i], rest: list};
+
+  }
