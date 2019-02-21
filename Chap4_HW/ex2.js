baa007ef970aac81eb32860276d4b8a3799fd012
Zach Freund <z148f106@ku.edu>
2019-02-21 00:59:55 -0600

    ex3 added into ex2 for now

--- Chap4_HW/ex2.js
+++ Chap4_HW/ex2.js
@@ -19,3 +19,15 @@ function range(x, y, incr = 1){
   }
   return(arr);
 }
+//ex3
+// Your code here.
+function nth(list, num){
+  if(num === 0 && list){
+    return(list.value);
+  }else if(list.rest){
+    return(nth(list.rest, num -1));
+  }
+}
+function prepend(element, subList){
+  let list = {value:
+function arrayTolist(...arr){
