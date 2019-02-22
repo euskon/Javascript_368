6e156d15aa729cff27a2769b7f95ee3f1124ac1a
Zach Freund <theeuskon@gmail.com>
2019-02-22 11:47:38 -0600

    fixed refactored

--- Chap4_HW/ex3LinkedList.js
+++ Chap4_HW/ex3LinkedList.js
@@ -1,20 +1,35 @@
-//ex3
-// Your code here.
 function nth(list, num){
-  if(num === 0 && list){
-    return(list.value);
-  }else if(list.rest){
-    return(nth(list.rest, num -1));
+  //console.log("list is:",list, "num is", num)
+  //console.log("typeof num", typeof num)
+  if(num === 0){
+    console.log("num is zero!!")
+    return list.value;
   }
+  if(list.rest){
+
+    return nth(list.rest, num -1);
+  }
+
+  if(num < 0){
+    console.log("Invalid Lissst Size");
+    return;
+  }
+  console.log("Invalid Lissst Size");
+  return;
 }
-arr1 = [5,6]
+
 function prepend(element, subList){
   let new_root = {value: element, rest: subList};//rest: {subList}
   return new_root;
 }
-function arrayTolist(arr){
-  let list = null;
-  for(let i = (arr.length - 1); i >= 0; i--){
+function arrayToList(arr){
+  //console.log(arr);
+  let list = {}
+  for(let i = (arr.length - 1); i >= 0; i --){
+    //console.log(arr[i]);
      let node = {value: arr[i], rest: list};
-
-  }
+     list = node;
+	}
+  console.log("here is my final list:", list)
+  return list;
+}
